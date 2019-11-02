db23e5e4100545a3bcc818be952d0a1fb0b65fa0
Steven Sheffey <srs6p@mtmail.mtsu.edu>
2019-11-02 11:17:30 -0500

    Added many things

--- server/main.py
+++ server/main.py
@@ -1,19 +1,26 @@
 #!/usr/bin/env python3
 
 from PIL import Image
-from sanic import Sanic
-from sanic.response import json
+from sanic import Sanic, response
 
+# ======================
+# Define the app
+# ======================
 app = Sanic()
 
 # Serve index.html statically
 app.static("/", "../static")
 
 
-@app.route('/upload')
+@app.route('/transform')
 async def test(request):
-    return json({'owo': 'world'})
+    print("received trans request")
+    return await response.file('../static/uwuwu.jpg')
 
 
+# ============================
+# Run the app
+# ============================
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
+    ssl = {'cert': "certs/MyCertificate.crt", 'key': "certs/MyKey.key"}
+    app.run(host='0.0.0.0', port=3000, ssl=ssl)
--- static/main.js
+++ static/main.js
@@ -1,7 +1,22 @@
-
-function main() {
-    let canvas = document.getElementById("render");
-    let ctx = canvas.getContext("2d");
+async function main() {
+    // Initialize canvas
+    const canvas = document.getElementById('render');
+    const ctx = canvas.getContext("2d");
+    // Initialize the video element
+    const video = document.getElementById("video"); 
+    // Open the stream from the webcam
+    const stream = await navigator
+        .mediaDevices
+        .getUserMedia({ video: true });
+    // Set the video element to stream the stream from the webcam
+    video.srcObject = stream;
+    // Play the stream
+    await video.play();
+    // Render the frame to the stream
+    ctx.drawImage(
+      video,
+      0, 0,
+      video.offsetWidth, video.offsetHeight
+    );
 }
-
-main();
+(async () => {await main()})()
